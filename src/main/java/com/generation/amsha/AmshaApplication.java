613f2e88adbba7e001eb8671fefe3230f8ecd5fa
Mbogo-anonymous <mbogoalex3@gmail.com>
2024-09-25 07:55:34 +0300

    initial commit

---
+++ src/main/java/com/generation/amsha/AmshaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.amsha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmshaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmshaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/amsha/AmshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.amsha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmshaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
