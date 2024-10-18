71acee90d1190c3a2c37fb2db8328d8abae2e929
Mbogo-anonymous <mbogoalex3@gmail.com>
2024-10-18 06:30:03 +0300

    config

--- src/main/java/com/generation/amsha/aws/config/WebConfig.java
+++ src/main/java/com/generation/amsha/aws/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.generation.amsha.aws.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
